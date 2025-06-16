c97ff067e89b4dbb078bfd98ce257ae2058bdcbc
apreezofficial <apreezofficial@gmail.com>
2025-06-16 15:47:49 +0100

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'conn.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="en" class="<?php echo $currentTheme === 'dark' ? 'dark' : ''; ?>">
+<html lang="en" class="light">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
