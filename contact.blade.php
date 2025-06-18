9b44be944da326f561563efa52b680c89d49a3ec
apreezofficial <apreezofficial@gmail.com>
2025-06-18 09:34:49 +0100

    update

--- contact.blade.php
+++ contact.blade.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Contact Draje</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
+
 <body class="bg-black text-white min-h-screen flex items-center justify-center p-6 transition-colors duration-300" id="body">
 
     <!-- Mode Toggle -->
